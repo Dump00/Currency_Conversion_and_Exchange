75715688b67e270a1eaa063d4318c7c15bceac7d
sovisrushain <sovisrushain@gmail.com>
2022-09-26 11:32:27 +0530

    apply JPA

--- currency-exchange-service/src/main/java/com/cisco/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/cisco/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,17 +1,24 @@
 package com.cisco.currencyexchangeservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
+@Entity(name = "currency_exchange")
 @NoArgsConstructor
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
+    @Column(name = "conversion_multiple")
     private BigDecimal conversionMultiple;
     private String environment;
 
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10001, 'USD', 'LKR', 350, '');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10002, 'EUR', 'LKR', 360, '');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10003, 'AUD', 'LKR', 250, '');
