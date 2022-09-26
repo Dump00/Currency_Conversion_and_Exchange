c6153580a385a561316f5b0473d8e1c8c07a0a3a
sovisrushain <sovisrushain@gmail.com>
2022-09-26 15:25:40 +0530

    invoke currency exchange from currency conversion service

--- currency-conversion-service/src/main/java/com/cisco/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cisco/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,11 +1,15 @@
 package com.cisco.currencyconversionservice.controller;
 
 import com.cisco.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,6 +18,23 @@ public class CurrencyConversionController {
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+        Map<String, String> uriVaribles = new HashMap<>();
+        uriVaribles.put("from", from);
+        uriVaribles.put("to", to);
+
+        /* Call currency-exchange-service */
+        ResponseEntity<CurrencyConversion> response = new RestTemplate()
+                                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                                                    CurrencyConversion.class,
+                                                    uriVaribles);
+
+        CurrencyConversion cc = response.getBody();
+
+        return new CurrencyConversion(cc.getId(), from, to, quantity,
+                cc.getConversionMultiple(),
+                quantity.multiply(cc.getConversionMultiple()),
+                cc.getEnvironment());
+
     }
 }
