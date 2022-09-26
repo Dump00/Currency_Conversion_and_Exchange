d10a1debe06ad660a25a3fb2ca76ccc3a81b248f
sovisrushain <sovisrushain@gmail.com>
2022-09-26 14:42:17 +0530

    implement repository lyr

--- currency-exchange-service/src/main/java/com/cisco/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/cisco/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,13 +1,13 @@
 package com.cisco.currencyexchangeservice.controller;
 
 import com.cisco.currencyexchangeservice.model.CurrencyExchange;
+import com.cisco.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
@@ -15,9 +15,12 @@ public class CurrencyExchangeController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        var ce = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(350));
+        CurrencyExchange ce = repository.findByFromAndTo(from, to);
         ce.setEnvironment(environment.getProperty("local.server.port"));
         return ce;
     }
---
+++ currency-exchange-service/src/main/java/com/cisco/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.cisco.currencyexchangeservice.repository;
+
+import com.cisco.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
