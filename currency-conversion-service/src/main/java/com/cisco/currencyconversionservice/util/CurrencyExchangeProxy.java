c0366fd86b7a552e0a01908ba25bbb2e73ac0669
sovisrushain <sovisrushain@gmail.com>
2022-09-27 11:17:26 +0530

    init api-gateway

--- currency-conversion-service/src/main/java/com/cisco/currencyconversionservice/util/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/cisco/currencyconversionservice/util/CurrencyExchangeProxy.java
@@ -5,7 +5,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+
+
+/* client side load balancing (happening through feign) */
+/* load balancer component */
+/* Feign client talks to eureka and pick up the instances of currency-exchange and do load balancing
+* between them */
+
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
